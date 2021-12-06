544c785bb8840cbcbea292c1d90f664976123310
Schattenbrot <aaron.machill@web.de>
2021-12-06 17:01:37 +0100

    added dockerfile for deployment

--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,20 @@ export default function Home() {
   const [url, setUrl] = useState('');
   const [source, setSource] = useState('');
 
-  useEffect(async () => {
-    newImageHandler();
-  }, []);
+  useEffect(() => {
+    const imageHandler = async () => {
+      newImageHandler();
+    }
+    imageHandler();
+  }, [])
+  // useEffect(async () => {
+  //  newImageHandler();
+  //}, []);
 
   const newImageHandler = async () => {
-    const result = await useFetch((data) => {
-      return { url: data.url, source: data.source_url };
-    });
+    const response = await fetch('https://nekos.best/api/v1/nekos');
+    const data = await response.json();
+    const result = { url: data.url, source: data.source_url };
 
     setUrl(result.url);
     setSource(result.source);
